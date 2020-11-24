f0414e8a0780e200ee3fc6bd2b99335165b86ca3
Karmo <karmorg@gmail.com>
2020-11-24 15:36:58 +0200

    application

---
+++ src/main/java/com/example/demo/VehstoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehstoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehstoryApplication.class, args);
+	}
+
+}
